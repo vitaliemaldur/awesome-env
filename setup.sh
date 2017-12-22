01b2cfc120ff8b14354ace3d4fb1a781a6fc2d50
Vitalie Maldur <maldur.vitalie@gmail.com>
2017-12-22 16:38:00 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Setup script for my awesome environment
+
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install packages
+brew install git tmux
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
