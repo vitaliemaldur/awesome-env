140633b4ed6cef4e30cb908862c9a07d0f8161f1
Vitalie Maldur <1472338+vitaliemaldur@users.noreply.github.com>
2018-08-21 13:18:32 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 # Setup script for my awesome environment
-
+brew update
 
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install packages
-brew install git tmux
+brew install git tmux pyenv pyenv-virtualenv
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
